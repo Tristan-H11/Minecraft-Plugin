078c9bd185b6cd35d361c7a7370ba677265d2c3b
Lucas <lucas.menke98@gmail.com>
2022-01-08 14:37:15 +0100

    Test Health

---
+++ src/main/java/com/github/tristanh11/ourownplugin/commands/Heal.java
@@ -0,0 +1,17 @@
+package com.github.tristanh11.ourownplugin.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Heal implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            ((Player) sender).setHealth(10);
+            return true;
+        }
+        return false;
+    }
+}
