e8833cc9e54b96299bdbc5e1db696d57f6da0ffc
Felefix201 <lucas.menke98@gmail.com>
2022-01-09 15:58:16 +0100

    Health optimiert

--- src/main/java/com/github/tristanh11/ourownplugin/commands/Health.java
+++ src/main/java/com/github/tristanh11/ourownplugin/commands/Health.java
@@ -1,5 +1,6 @@
 package com.github.tristanh11.ourownplugin.commands;
 
+import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,7 +10,10 @@ public class Health implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
-            ((Player) sender).setHealth(22.0);
+
+            Player player = (Player) sender;
+            double maxHealth = player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+            player.setHealth(maxHealth);
             sender.sendMessage("You are healed");
             return true;
         }
